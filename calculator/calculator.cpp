2c3d906db3964b5abfc0896365680a72c8b5b7c2
Konstantin Dashkevich <dashkevichpy@gmail.com>
2024-12-24 11:43:47 +0700

    Все работает, комит перед отправкой на ревью

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,131 @@
-// В этом файле определения функций.
-// За основу возьмите решение предыдущей задачи.
+#include "calculator.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+//Читаем число
+bool ReadNumber(Number& result) {
+    return (std::cin >> result) ? true : (std::cerr << "Error: Numeric operand expected" << std::endl, false);
+}
+
+//Читаем операцию
+bool ReadChar(std::string& ch){
+    std::vector<std::string> symbol = {"*", "+", "-", "/", "**", "q", "=", ":", "c", "s", "l"};
+    if(std::cin >> ch){
+        auto indx = std::find(symbol.begin(), symbol.end(), ch);
+        if(indx != symbol.end()){
+            return true;
+        }else{
+            std::cerr << "Error: Unknown token " << ch << std::endl;
+            return false;
+        }
+
+    }else{
+        std::cerr << "Error: Unknown token " << ch << std::endl;
+        return false;
+    }
+}
+
+//Арифметические функции
+void Add(Number& x, Number y){
+    x += y;
+}
+
+void Subtract(Number& x, Number y){
+    x -= y;
+}
+
+void Multiply(Number& x, Number y){
+    x *= y;
+}
+
+void Divide(Number& x, Number y){
+    x/=y;
+}
+
+void Power(Number& x, Number y){
+    x = std::pow(x, y);
+}
+
+
+//Функции калькултора
+void QuitCalculator() {
+    exit(0);
+}
+
+void ShowResult(Number& x){
+    std::cout << x << std::endl;
+}
+
+void SetNumber(Number& x, Number y){
+    x = y;
+}
+
+void ZeroX(Number& x){
+    x = 0;
+}
+
+void SaveNumber(Number& x, Number& s, bool& saveNumber){
+    s = x;
+    saveNumber = true;
+}
+
+void LoadFromMemory(Number& s, Number& x, bool& saveNumber){
+    if(saveNumber){
+        x = s;
+    }else{
+        std::cerr << "Error: Memory is empty" << std::endl;
+        exit(0);
+    }
+}
+
+//Выбор операции
+bool SelectOperation(Number& x, Number& y, Number& s, std::string& ch, bool& saveNumber){
+    if (ch == "*" || ch == "+" || ch == "-" || ch == "/" || ch == "**" || ch == ":") {
+        if (!ReadNumber(y)) {
+            return false;
+        }
+    }
+    if(ch == "*"){
+        Multiply(x, y);
+    }else if(ch == "+"){
+        Add(x, y);
+    }else if(ch == "-"){
+        Subtract(x, y);
+    }else if(ch == "/"){
+        Divide(x, y);
+    }else if(ch == "**"){
+        Power(x, y);
+    }else if(ch == "q"){
+        QuitCalculator();
+    }else if(ch == "="){
+        ShowResult(x);
+    }else if(ch == ":"){
+        SetNumber(x, y);
+    }else if(ch == "c"){
+        ZeroX(x);
+    }else if(ch == "s"){
+        SaveNumber(x, s, saveNumber);
+    }else if(ch == "l"){
+        LoadFromMemory(s, x, saveNumber);
+    }
+    return true;
+}
+
+//Основной цикл
+void RunCalculatorCycle(){
+    std::string ch;
+    Number x, y, s;
+    bool work = true, saveNumber = false;
+    if(!ReadNumber(x)){
+        return;
+    }
+    while (work) {
+        if(!ReadChar(ch)){
+            break;
+        }
+        work = SelectOperation(x, y, s, ch, saveNumber);
+    }
+}
--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,1 +1,26 @@
-// В этом файле объявления функций.
+#include <string>
+
+using Number = double;
+
+// Функции чтения
+bool ReadNumber(Number& result);
+bool ReadChar(std::string& ch);
+
+// Арифметические операции
+void Add(Number& x, Number y);
+void Subtract(Number& x, Number y);
+void Multiply(Number& x, Number y);
+void Divide(Number& x, Number y);
+void Power(Number& x, Number y);
+
+// Операции калькулятора
+void QuitCalculator();
+void ShowResult(Number& x);
+void SetNumber(Number& x, Number y);
+void ZeroX(Number& x);
+void SaveNumber(Number& y, Number& s, bool& saveNumber);
+void LoadFromMemory(Number& s, Number& y, bool& saveNumber);
+
+// Основной цикл и выбор операций
+bool SelectOperation(Number& x, Number& y, Number& s, std::string& ch, bool& saveNumber);
+void RunCalculatorCycle();
